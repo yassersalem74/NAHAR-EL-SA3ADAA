b6adace2e74e7bb970917682e02b3794dc42079b
yassersalem74 <yassersalem9099@gmail.com>
2025-12-29 10:25:00 +0200

    FEATURE/ added hall-reserve-form page

---
+++ js/book-form.js
@@ -0,0 +1,21 @@
+document.querySelectorAll(".check-box-container").forEach(card => {
+    const radio = card.querySelector('input[type="radio"]');
+
+    card.addEventListener("click", () => {
+        radio.checked = true;
+    });
+});
+
+radio.addEventListener("click", e => e.stopPropagation());
+
+
+document.querySelectorAll(".input-card").forEach(card => {
+    const input = card.querySelector("input");
+
+    card.addEventListener("click", () => {
+        input.focus();
+        input.showPicker && input.showPicker(); 
+    });
+});
+
+
